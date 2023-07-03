e5fb1a02fa03bc593552c917b426d838cfb1f915
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-07-03 21:50:26 +0000

    css and component created

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,3 +1,36 @@
 import react, { useState} from "react";
 
+const TrafficLight = () => {
 
+    const [color, setColor] = useState("null")
+
+
+    return (
+        <div className="container text-center">
+
+        <div className="top"></div>
+
+        <div className="traffic-ligth">
+            <div
+                className={
+                    color === "red" ? "red light selected" : "red light"
+                }
+                onClick={() => setColor("red")}></div>
+            <div
+                className={
+                    color === "yellow"
+                        ? "yellow light selected"
+                        : "yellow light"
+                }
+                onClick={() => setColor("yellow")}></div>
+            <div
+                className={
+                    color === "green"
+                        ? "green light selected"
+                        : "green light"
+                }
+                onClick={() => setColor("green")}></div>
+            </div>
+        </div>
+    )
+}
